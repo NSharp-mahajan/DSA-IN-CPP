f26046479b8e1f5858217a64a9ebdaf13abba54b
NSharp-mahajan <mahajannatansh@gmail.com>
2025-08-25 16:22:24 +0530

    Initialize

--- Arrays/Kadane's_Algorithm.cpp
+++ Arrays/Kadane's_Algorithm.cpp
@@ -1,20 +1,27 @@
-#include<iostream>
-#include<vector>
+// Brute Force Approach
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<climits>
 using namespace std;
 
-int main(){
-    int n = 5;
-    int arr[5] = {1,2,3,4,5};
-    int maxSum = INT_MIN;
-
-    for(int st = 0; st < n; st++){
-        int currSum = 0;
-        for(int end = st; end < n; end++){
-            currSum += arr[end];
-            maxSum = max(currSum, maxSum);
+int kadane(int arr[], int n){
+    int max_sum = INT_MIN;
+    for(int i = 0; i<n;i++){
+        int sum=0;
+        for(int j = i; j < n; j++){
+            sum += arr[j];
+            if(sum > max_sum){
+                max_sum = sum;
+            }
         }
     }
-
-    cout << "Max Subarray sum: " << maxSum << endl;
+    return max_sum;
+}
+int main() {
+    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = kadane(arr, n);
+    cout << "The maximum subarray sum (brute force) is: " << result << endl;
     return 0;
 }

6136afaf1d3f3cbad2a02d9d768daaa688631f88
NSharp-mahajan <mahajannatansh@gmail.com>
2025-09-15 12:09:55 +0530

    Made changes into stack and queue

--- Stack/Intro_to_stack.cpp
+++ Stack/Intro_to_stack.cpp
@@ -4,7 +4,7 @@ Queue follows FIFO (First in First Out)*/
 
 // Stack Using Arrays 
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 class Stack {
@@ -57,7 +57,7 @@ int main() {
     cout << "Size: " << st.size() << endl; 
 
     return 0;
-}
+}*/
 
 // Queue using arrays
 
@@ -134,3 +134,109 @@ int main() {
 
     return 0;
 }*/
+
+// Implementing stack using queues
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class MyStack {
+    queue<int> q;
+public:
+    void push(int x) {
+        int s = q.size();
+        q.push(x);
+        for (int i = 0; i < s; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    void pop() {
+        if (q.empty()) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        q.pop();
+    }
+    int top() {
+        if (q.empty()) {
+            cout << "Stack is empty\n";
+            return -1;
+        }
+        return q.front();
+    }
+    bool empty() {
+        return q.empty();
+    }
+};
+int main() {
+    MyStack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    cout << "Top: " << st.top() << endl; // 30
+    st.pop();
+    cout << "Top after pop: " << st.top() << endl; // 20
+
+    return 0;
+}
+
+// Implementing queues using stacks
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class MyQueue {
+    stack<int> s1, s2;
+public:
+    void push(int x) {
+        s1.push(x);
+    }
+    int pop() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
+            return -1;
+        }
+
+          if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+
+        int val = s2.top();
+        s2.pop();
+        return val;
+    }
+    int front() {
+        if (s1.empty() && s2.empty()) {
+            cout << "Queue is empty\n";
+            return -1;
+        }
+
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
+
+int main() {
+    MyQueue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    cout << "Front: " << q.front() << endl;
+    cout << "Popped: " << q.pop() << endl; 
+    cout << "Front now: " << q.front() << endl; 
+    return 0;
+}

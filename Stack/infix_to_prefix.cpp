7915133de69ba6300fe3d9a027200a3506f18a1a
NSharp-mahajan <mahajannatansh@gmail.com>
2025-09-19 00:45:28 +0530

    Infix to prefix added

---
+++ Stack/infix_to_prefix.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int precedence(char c) {
+    if (c == '^') return 3;
+    else if (c == '*' || c == '/') return 2;
+    else if (c == '+' || c == '-') return 1;
+    else return -1;
+}
+
+string infixToPrefix(string s) {
+    reverse(s.begin(), s.end());
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(') {
+            s[i] = ')';
+        }
+        else if (s[i] == ')') {
+            s[i] = '(';
+        }
+    }
+
+    stack<char> st;
+    string result;
+
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')) {
+            result += c;
+        }
+        else if (c == '(') {
+            st.push(c);
+        }
+        else if (c == ')') {
+            while (!st.empty() && st.top() != '(') {
+                result += st.top();
+                st.pop();
+            }
+            if (!st.empty()) st.pop();
+        }
+        else {
+            while (!st.empty() && precedence(st.top()) >= precedence(c)) {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+int main() {
+    string infix = "(A-B/C)*(A/K-L)";
+    cout << "Infix Expression: " << infix << endl;
+    cout << "Prefix Expression: " << infixToPrefix(infix) << endl;
+    return 0;
+}

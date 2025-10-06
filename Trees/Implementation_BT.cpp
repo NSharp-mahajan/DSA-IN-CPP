77f9fd546685371bf12cfef96e4b9c8d78ef2ac1
NSharp-mahajan <mahajannatansh@gmail.com>
2025-10-06 23:43:23 +0530

    Added 2 basic trees questions

--- Trees/Implementation_BT.cpp
+++ Trees/Implementation_BT.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 struct Node {
@@ -22,4 +22,72 @@ int main() {
     cout << "Child Value: " << root -> left -> right -> data << endl;
 
     return 0;
+}*/
+
+// ------------------Basic Questions ---------------------//
+/*
+        1
+       / \         Tasks to perform:Print root node’s data, Print its left and right children, Count total nodes recursively
+      2   3
+     / \
+    4   5
+*/
+
+/*#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main() {
+    // Create nodes
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout << "Root: " << root->data << endl;
+    cout << "Left Child of Root: " << root->left->data << endl;
+    cout << "Right Child of Root: " << root->right->data << endl;
+    return 0;
+}*/
+
+// Given the root of a binary tree, write a recursive function to find the height of the above given tree.
+
+/*#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+int findHeight(Node* root){
+    if(root == NULL)return 0;
+    int leftH = findHeight(root->left);
+    int rightH = findHeight(root->right);
+    return max(leftH, rightH)+1;
 }
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    cout << "Height of Tree: " << findHeight(root);
+    return 0;
+}*/
+

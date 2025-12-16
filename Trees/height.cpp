b984e902afda593bb08916ffba67559411b29a4b
NSharp-mahajan <mahajannatansh@gmail.com>
2025-12-17 00:09:35 +0530

    Question added

--- Trees/height.cpp
+++ Trees/height.cpp
@@ -37,9 +37,29 @@ int height(Node* root){
     return max(leftHeight, rightHeight) + 1;
 }
 
+int count(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftCount = count(root->left);
+    int rightCount = count(root->right);
+    return leftCount + rightCount +1;
+}
+
+int sum(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    int leftSum = sum(root->left);
+    int rightSum = sum(root->right);
+    return leftSum + rightSum + root->data;
+}
+
 int main(){
     vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node* root = buildTree(preOrder);
-    cout << "Height: " << height(root) << endl;
+    // cout << "Height: " << height(root) << endl;
+    // cout << "Count: " << count(root) << endl;
+    cout << "sum: "<< sum(root) << endl;
     return 0;
 }
